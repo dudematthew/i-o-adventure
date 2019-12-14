8b0628932595fd0d8683aadf14a996988bb1db34
Mateusz <mmoczydlowski4@gmail.com>
2019-12-14 16:14:47 +0100

    First attempt to create a MapGenerator class

--- MapGenerator/MapGenerator.h
+++ MapGenerator/MapGenerator.h
@@ -1,17 +1,30 @@
-class MapGenerator
-{
-public:
-	MapGenerator(int size);
-	int GetSize();
-private: 
-	int size;
-};
+namespace MapGenerator {
+	/*
+	* Class that generates array, that represents
+	* the structure of the map based on parameters
+	*/
+	class MapGenerator {
+	public:
+		int size;
 
-MapGenerator::MapGenerator(int size) {
-	this->size = size;
-}
+		MapGenerator(int size);
+		void GenerateMap();
+	private:
+		
+	};
 
-int MapGenerator::GetSize() {
-	return this->size;
+	/*
+	* Constructor
+	* int size: size of generated map
+	*/
+	MapGenerator::MapGenerator(int size = 10) {
+		this->size = size;
+	}
+
+	void MapGenerator::GenerateMap () {
+
+	}
+
+
 }
 

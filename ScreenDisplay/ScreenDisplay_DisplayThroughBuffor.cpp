43ba7f515ec8ab7f1d58a627f43fa4d0383b7211
Przemek Polek <poloneyo1990@gmail.com>
2020-01-17 14:57:38 +0100

    Modified Setchar and added getid

---
+++ ScreenDisplay/ScreenDisplay_DisplayThroughBuffor.cpp
@@ -0,0 +1,26 @@
+#include "ScreenDisplay.h"
+
+void ScreenDisplay::DisplayThroughBuffor(vector <vector <vector <int>>> map)
+{
+	ScreenDisplay size;
+	int bL = size.getScreenWidth();
+	vector <vector <vector <int>>> p;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	int mH = map.size();
+	int mW = map[0].size();
+	unsigned char* data = new unsigned char[mH * bL];
+
+	for (int i = 0; i < mH; i++) {
+
+		for (int j = 0; j < mW; j++) {
+			if(!(map[i][j][0] == 0))
+				data[j + (i * bL)] = (char)(map[i][j][0]);
+			else
+				data[j + (i * bL)] = (char)(map[i][j][1]);
+		}
+		for (int j = mW; j < bL; j++) {
+			data[j + (i * bL)] = ' ';
+		}
+	}
+	WriteFile(handle, data, sizeof(char) * mH * bL, NULL, NULL);
+}

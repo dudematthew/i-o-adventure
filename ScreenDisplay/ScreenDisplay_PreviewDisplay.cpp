ddef20c12b320ee2990d18c799f321f54d30f652
Mateusz <mmoczydlowski4@gmail.com>
2020-01-21 00:01:54 +0100

    Changes to project references
    Deleted unnecessary comments

--- ScreenDisplay/ScreenDisplay_PreviewDisplay.cpp
+++ ScreenDisplay/ScreenDisplay_PreviewDisplay.cpp
@@ -1,8 +1,7 @@
 #include "ScreenDisplay.h"
+#include "MapEntity.h"
 #include <iostream>
 #include <conio.h>
-// w czym problem
-// go lower
 
 void ScreenDisplay::PreviewDisplay()
 {
@@ -91,27 +90,12 @@ void ScreenDisplay::PreviewDisplay()
 	};
 
 	ScreenDisplay b;
-	vector <MapEntity> entities; // tu bam nie istnieje  no tu jest problem tylko tutaj jest problem
-	// tak swoj¹ drog¹ (kurwa) utworzy³eœ PUSTY wektor obiektów MapEntity o nazwie enTITIES. 
-	// w³aœnie widzisz w czym jest problem
-	// ScreenDisplay nie widzi MapEntity
-	// TYLKO o to mi chodzi
-	// teraz fajnie by³oby go zadeklarowaæ co nie
-	// mordo to tak nie dzia³a mordo
-	// nie pojeba³o mi siê z c# bo tam jest = new MapEntity(), kurwa tu te¿ tak jest
-	// mordo ale ten obiekt przecie¿ jest w map enTITTY 
-	// co Ty pierdolisz
-	// ocipia³eœ kurwa XD
-	// no ocipia³em  w chuj tablicy siê nie definiuje kurwa debil
-	// nie mogê zagl¹daæ do projektów wiêc spushuj to gówno razem z komentarzami i napierdalam k 
-
-	b.setScreenTitle(L"I/O Adventure"); // czy problem jest ¿e map entity nie jest zdefiniowany? si mordo myœmy to robili 
+	vector <MapEntity> entities;
+	b.setScreenTitle(L"I/O Adventure");
 	b.setScreenFont(24);
 	b.setScreenSize(NULL, NULL);
 	do {
-		b.DisplayThroughBuffor(vect, entities); // KURWA XDDDDDDDDD
-		// lepsze ni¿ ch³op za babe przebran o kurwa ale siê zjeba³o na chwilê NO ej no nie mogê znaleŸæ
-		// tej opcji
+		b.DisplayThroughBuffor(vect);
 
 		cout << "Continue? 1/0";
 		char c = _getch();

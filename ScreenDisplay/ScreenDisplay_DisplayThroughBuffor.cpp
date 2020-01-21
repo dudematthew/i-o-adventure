0d04f6fa4708dbbd0c08aaeceb24a7d13c7320c9
Przemek Polek <poloneyo1990@gmail.com>
2020-01-21 15:39:56 +0100

    Turn off using precompiled headers

--- ScreenDisplay/ScreenDisplay_DisplayThroughBuffor.cpp
+++ ScreenDisplay/ScreenDisplay_DisplayThroughBuffor.cpp
@@ -1,26 +1,28 @@
+#include "pch.h"
 #include "ScreenDisplay.h"
 
-void ScreenDisplay::DisplayThroughBuffor(vector <vector <vector <int>>> map)
+void ScreenDisplay::DisplayThroughBuffor(vector <vector <vector <int>>> map, vector <MapEntity> entities)
 {
-	ScreenDisplay size;
+	/*ScreenDisplay size;
+
 	int bL = size.getScreenWidth();
 	vector <vector <vector <int>>> p;
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	int mH = map.size();
-	int mW = map[0].size();
-	unsigned char* data = new unsigned char[mH * bL];
+	unsigned char* data = new unsigned char[(long)(map.size() * bL)];
 
-	for (int i = 0; i < mH; i++) {
+	for (int i = 0; i < map.size(); i++) {
 
-		for (int j = 0; j < mW; j++) {
-			if(!(map[i][j][0] == 0))
-				data[j + (i * bL)] = (char)(map[i][j][0]);
-			else
+		for (int j = 0; j < map[0].size(); j++) {
 				data[j + (i * bL)] = (char)(map[i][j][1]);
 		}
-		for (int j = mW; j < bL; j++) {
+		for (int j = static_cast<int>(map[0].size()); j < bL; j++) {
 			data[j + (i * bL)] = ' ';
 		}
 	}
-	WriteFile(handle, data, sizeof(char) * mH * bL, NULL, NULL);
+
+	for (int i = 0; i < entities.size(); i++) {
+		data[entities[i].GetCoordinates().X + (entities[i].GetCoordinates().Y * bL)] = (char)(entities[i].GetChar());
+	}
+
+	WriteFile(handle, data, sizeof(char) * map.size() * bL, NULL, NULL);*/
 }
---
+++ ScreenDisplay/ScreenDisplay_PreviewDisplay.cpp
@@ -0,0 +1,115 @@
+#include "pch.h"
+#include "ScreenDisplay.h"
+#include "MapEntity.h"
+#include <iostream>
+#include <conio.h>
+
+void ScreenDisplay::PreviewDisplay()
+{
+	/*vector < vector < vector < int > > > vect
+	{  
+		{
+			{ 196 , 219 },
+			{ 171 , 219 },
+			{ 172 , 219 },
+			{ 210 , 219 },
+			{ 208 , 219 },
+			{ 220 , 219 },
+			{ 218 , 219 },
+			{ 210 , 219 }
+		},
+		{
+			{ 197 , 219 },
+			{ 197 , 219 },
+			{ 197 , 219 },
+			{ 197 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		},
+		{
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 },
+			{ 0 , 219 }
+		}
+	};*/
+
+	/*ScreenDisplay b;*/
+	MapEntity previewEntity(0);
+	/*COORD previewCoords;
+	previewCoords.X = 1;
+	previewCoords.Y = 1;
+	previewEntity.SetCoordinates(previewCoords);
+	previewEntity.SetChar(49);*/
+	
+	/*vector <MapEntity> entities;
+	b.setScreenTitle(L"I/O Adventure");
+	b.setScreenFont(24);
+	b.setScreenSize(NULL, NULL);
+	do {
+		b.DisplayThroughBuffor(vect, entities);
+
+		cout << "Continue? 1/0";
+		char c = _getch();
+		if (c != '1')
+			break;
+		b.ClearConsole();
+	} while (true);
+	return;*/
+}
